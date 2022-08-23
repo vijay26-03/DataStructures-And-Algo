1ba9c2bab695b429d1f98fca3dbd83994d57e51e
vijay26-03 <61930635+vijay26-03@users.noreply.github.com>
2022-08-23 10:22:40 +0530

    Update word_game.py

--- word_game.py
+++ word_game.py
@@ -1,11 +1,13 @@
-inpt=list(input().split())
+inpt=['a','b','c','d','e','f','g','h','i','j','k']
 index=0
 pointer_count=1
-while(len(inpt)>1):
-    if(index>=len(inpt)):
-        index=-1
-    if(pointer_count%4==0 or pointer_count%10==4):
+while len(inpt)!=1:
+    if pointer_count%4==0 or pointer_count%10==4:
         inpt.remove(inpt[index])
-    index+=1
+        index+=1
+    if index >=len(inpt)-1:
+        index=0
+    else:
+        index+=1
     pointer_count+=1
 print(inpt)
